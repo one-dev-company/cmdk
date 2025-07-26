48519f894008f9ce8df187a126f909f04d7dbcbe
Danilo Barros <danilo.barros@seplag.ce.gov.br>
2025-07-26 15:41:22 -0300

    chore(tsup.config): improve condig file

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  outDir: 'dist',
   clean: true,
   dts: true,
   format: ['cjs', 'esm'],
-  minify: false,
-  sourcemap: true,
-  splitting: false,
+  minify: true,
+  sourcemap: false,
+  loader: {
+    '.js': 'jsx',
+  },
 })
 
